c9d43015089a87fe00ec894e4e9713d598084e52
Arash <arash.hosseinabady@gmail.com>
2025-08-29 12:37:47 +0330

    drop field from table if removed in model

--- db/migration.go
+++ db/migration.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"gorm.io/gorm"
 	"log"
 	"reportly/model"
 )
@@ -9,6 +10,25 @@ import (
 func RegisterModels() []interface{} {
 	return []interface{}{
 		&model.ReportRequest{},
+	}
+}
+
+func dropUnusedColumns(db *gorm.DB, model interface{}) {
+	stmt := &gorm.Statement{DB: db}
+	_ = stmt.Parse(model)
+
+	tableName := stmt.Schema.Table
+
+	// get all current fields
+	columns, _ := db.Migrator().ColumnTypes(model)
+
+	for _, col := range columns {
+		colName := col.Name()
+		// remove field that not exists in a model
+		if stmt.Schema.LookUpField(colName) == nil {
+			log.Printf("Dropping unused column: %s.%s", tableName, colName)
+			_ = db.Migrator().DropColumn(model, colName)
+		}
 	}
 }
 
@@ -26,6 +46,8 @@ func RunMigration() {
 			log.Printf("Migration failed for table '%s': %v", tn, err)
 			continue
 		}
+
+		dropUnusedColumns(db, m)
 
 		log.Printf("Migrated table: %s", tn)
 	}
